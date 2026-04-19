37acbf1290a931b5419929cb4423bb4cbb0c01ad
katirant <katirant01@gmail.com>
2026-04-19 21:03:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const config = require('./config.json');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.MessageContent
+    ],
+    partials: [Partials.Channel, Partials.Message]
+});
+
+let setupData = {};
+const setupPath = path.join(__dirname, 'setup.json');
+if (fs.existsSync(setupPath)) {
+    setupData = JSON.parse(fs.readFileSync(setupPath, 'utf8'));
+}
+
+client.db = {
+    ticketData: new Map(),
+    channelMap: new Map(),
+    config: setupData
+};
+
+client.commands = new Collection();
+
+const eventsPath = path.join(__dirname, 'src', 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
+    }
+}
+
+const commandsPath = path.join(__dirname, 'src', 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    client.commands.set(command.data.name, command);
+}
+
+client.login(config.token);
